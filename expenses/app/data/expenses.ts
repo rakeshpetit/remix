f5cf3726f410b93c3910674a1c5994a6c8d3d253
Rakesh Arunachalam <rakesh@thoughtbot.com>
2022-12-24 17:58:35 +0000

    Add DUMMY_EXPENSES file
    - This file will be replaced later to use expenses from the database.

---
+++ expenses/app/data/expenses.ts
@@ -0,0 +1,22 @@
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2022, 11, 14).toISOString(),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2022, 10, 11).toISOString(),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2022, 8, 24).toISOString(),
+  },
+];
+
+export { DUMMY_EXPENSES };
